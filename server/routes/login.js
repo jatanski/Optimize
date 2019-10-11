acc02df859c3c4f35dd894aee10e6f527a1aa5a8
b-robak <bartosz.robak@hotmail.com>
2019-10-11 21:52:51 +0200

    login fix

--- server/routes/login.js
+++ server/routes/login.js
@@ -16,7 +16,7 @@ router.post('/', async (req, res) => {
         const validPassword = await bcrypt.compare(req.body.password, user.password);
         if (!validPassword) return res.status(400).send('Invalid email or password.');
 
-        const token = await user.createToken();
+        const token = await user.generateAuthToken();
 
         res.header('x-auth-token', token).send({
             _id: user._id,
