e49cb5e144cf02d184b6426bf78eed66086b86fa
Paweł <praczykp2705@vp.pl>
2019-10-11 22:18:31 +0200

    Added thread schema

--- server/startup/routes.js
+++ server/startup/routes.js
@@ -2,14 +2,14 @@ const express = require('express');
 const error = require('../middleware/error');
 const usersRoute = require('../routes/users')
 const loginRouter = require('../routes/login');
+//const threadsRoute = require('../routes/threads');
 
 module.exports = function (app) {
 
     app.use(express.json());
 
     app.use('/api/users', usersRoute);
-
-    app.use(error);
-
+   // app.use('/api/threads', threadsRoute);
     app.use('/api/login', loginRouter);
+    app.use(error);
 };
