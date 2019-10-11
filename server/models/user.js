5bec3ceee06ecce3bb19bcad5a5218c4c9f6d643
Paweł <praczykp2705@vp.pl>
2019-10-11 21:23:03 +0200

    Added register for user

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,8 @@
-const mongose = require('mongoose');
+const mongoose = require('mongoose');
 const Joi = require('@hapi/joi');
+const config = require('config');
 
-const userSchema = new mongose.Schema({
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -12,7 +13,8 @@ const userSchema = new mongose.Schema({
         type: String,
         required: true,
         minlength: 5,
-        maxlength: 50
+        maxlength: 50,
+        default: "Mac"
     },
     avatar: {
         type: String,
@@ -27,6 +29,12 @@ const userSchema = new mongose.Schema({
         minlength: 5,
         maxlength: 255,
     },
+    slackId: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+    },
     password: {
         type: String,
         required: true,
@@ -34,12 +42,44 @@ const userSchema = new mongose.Schema({
         maxlength: 1024,
     },
     team_role: {
-        type: String, 
+        type: String,
         required: true,
-        enum: [Admin, Moderator, User]
+        enum: ["Admin", "Moderator", "User"]
     },
     teams: {
         type: Array,
         required: true,
     },
-})
+});
+
+
+function validateUser(user) {
+  const schema = {
+    name: Joi.string()
+      .min(5)
+      .max(50)
+      .required(),
+    avatar: 'https://osclass.calinbehtuk.ro/oc-content/themes/vrisko/images/no_user.png',
+    email: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(5)
+      .max(1024)
+      .required()
+  };
+
+  return Joi.validate(user, schema);
+}
+
+
+userSchema.methods.generateAuthToken = function() {
+  const token = jwt.sign({ _id: this._id }, config.get('jwtPrivateKey'));
+  return token;
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports.User = User;
--- server/startup/routes.js
+++ server/startup/routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const error = require('../middleware/error');
+const usersRoute = require('../routes/users')
 
 module.exports = function (app) {
     app.use(express.json());
+
+    app.use('/api/users', usersRoute);
+
     app.use(error);
 };
