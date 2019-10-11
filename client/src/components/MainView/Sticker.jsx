da8548a3eaf9409f60d8b49a2ca02fa11470b3f7
Greg <gpr.witczak@gmail.com>
2019-10-12 00:42:00 +0200

    mainView2

--- client/src/components/MainView/Sticker.jsx
+++ client/src/components/MainView/Sticker.jsx
@@ -2,18 +2,38 @@ import React, { Component } from "react";
 import { MDBContainer, MDBCardHeader, MDBCardBody, MDBCardTitle, MDBCardText, MDBBtn, MDBCard } from "mdbreact";
 
 class Sticker extends Component {
+  constructor (props) {
+    super(props);
+    this.state = ({
+      category: props.category,
+      name: props.name,
+      content: props.content,
+    });
+  }
+
+  setColor = () => {
+    const category = this.state.category;
+
+    if (category === "Front") {
+      return 'amber'
+    } else if (category === "Back") {
+      return 'deep-orange'
+    } else {
+      return 'indigo'
+    }
+  }
 
   render = () => {
     return (
       <MDBContainer className="sticker">
         <MDBCard style={{ marginTop: "1rem" }}>
-          <MDBCardHeader color="deep-orange lighten-1">Frontend</MDBCardHeader>
+          <MDBCardHeader color={`${this.setColor()} lighten-1`}>{this.state.category}</MDBCardHeader>
           <MDBCardBody>
-            <MDBCardTitle>Titile of thread</MDBCardTitle>
+            <MDBCardTitle>{this.state.name}</MDBCardTitle>
             <MDBCardText>
-              Lorem ipsum.. Lorem ipsum.. Lorem ipsum.. Lorem ipsum.. Lorem ipsum..
+              {this.state.content}
             </MDBCardText>
-            <MDBBtn color="deep-orange">Go to thread</MDBBtn>
+            <MDBBtn color={this.setColor()}>Go to thread</MDBBtn>
           </MDBCardBody>
         </MDBCard>
       </MDBContainer>
