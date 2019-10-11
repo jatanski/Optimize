e4629477711bf74fa93ddcfac38ca8f36420ce22
Micavanco <micavanco@gmail.com>
2019-10-12 01:22:44 +0200

    Fixed adding service worker

--- client/src/index.js
+++ client/src/index.js
@@ -8,17 +8,44 @@ import App from './App';
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 
+const publicKey = "BJ56SiiWKsOytzDkAMsw3DwuoUBV3KLTAkeQr6uWLKmYqmqjmHkj5z8u3jiFImoDv5c3Nk869AfgWwdgmV1TrOY";
+
 if('serviceWorker' in navigator) {
     send().catch(err => console.log(err));
 }
 
 async function send() {
     console.log('Registering serviceWorker...');
-    const register = await navigator.serviceWorker.register('/worker.js', {
-        scope: '/'
+    const register = await navigator.serviceWorker.register('/sw.js', {
+        scope: '/',
+        headers: {'Content-Type': 'application/json'}
     });
     console.log('Registered serviceWorker...');
+    const subscription = await register.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(publicKey)
+    });
 
+    await fetch('http://localhost:8000/api/subscribe', {
+       method: 'POST',
+       body: JSON.stringify(subscription),
+       headers: {'Content-Type': 'application/json'}
+    });
+}
+
+function urlBase64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+        .replace(/-/g, '+')
+        .replace(/_/g, '/');
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
 }
 
 
