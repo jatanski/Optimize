b9f2502a305c0dd7e1c3ddecc3b61181ff85acd4
Kuba <ja.tanski@gmail.com>
2019-10-11 23:25:30 +0200

    add slackId

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,8 +2,8 @@ const _ = require("lodash");
 const bcrypt = require("bcrypt");
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 const { User, validate } = require("../models/user");
-  
 
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
@@ -13,15 +13,53 @@ router.post("/", async (req, res) => {
 
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User already registered.");
-    user = new User (_.pick(req.body, ['name', 'email', 'password', 'slackId']));
-    // Generate salt and hashed password
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(user.password, salt);
 
+  let slackId = "";
+
+  // download users list of slack workspace, find user and add his id to userObject
+  await axios
+    .post(
+      "https://slack.com/api/users.list",
+      {
+        token:
+          "xoxp-773287386577-781574553623-791353069511-1e0d1c14f0182f0d156e0619e2d78ea2"
+      },
+      {
+        headers: {
+          Authorization:
+            "Bearer xoxp-773287386577-781574553623-791353069511-1e0d1c14f0182f0d156e0619e2d78ea2"
+        }
+      }
+    )
+    .then(response => {
+      const members = response.data.members;
+
+      const user = members.filter(member => {
+        return member.name == req.body.slackName;
+      });
+
+      console.log(user[0].id);
+
+      slackId = user[0].id;
+    });
+
+  user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    slackId: slackId
+  });
+  user.teams.push("essa");
+
+  // Generate salt and hashed password
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+
+  console.log(user);
   await user.save();
   const token = user.generateAuthToken();
-  res.header('x-auth-token', token);
-    res.send(user);
+  res.header("x-auth-token", token);
+  res.send(user);
 });
 
 module.exports = router;
