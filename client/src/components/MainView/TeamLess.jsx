87c98ab01a92de971cdb361f3f44767a87e54d3d
Greg <gpr.witczak@gmail.com>
2019-10-12 09:37:13 +0200

    hot fix

--- client/src/components/MainView/TeamLess.jsx
+++ client/src/components/MainView/TeamLess.jsx
@@ -1,18 +1,29 @@
 import React, { Component } from "react";
 import { MDBCard, MDBCardBody, MDBContainer, MDBBtn } from "mdbreact";
+import {withRouter} from 'react-router';
 
 class TeamLess extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  onClickHandler = () => {
+    this.props.history.push('/home/projects');
+  }
 
   render = () => {
     return (
-      <MDBContainer style={{ marginTop: "1rem" }}>
+      <React.Fragment>
+        <div className="main-content__top-bar">Dashboard</div>
+      <MDBContainer style={{ marginTop: "4rem" }}>
         <MDBCard className="text-center team-less-card">
           <MDBCardBody>You are not in any team yet!</MDBCardBody>
-          <MDBBtn outline color="deep-orange" className="w-50 p3" >Create Team</MDBBtn>
+          <MDBBtn onClick={this.onClickHandler} outline color="deep-orange" className="w-50 p3" >Create Project</MDBBtn>
         </MDBCard>
       </MDBContainer>
+      </React.Fragment>
     );
   };
 }
 
-export default TeamLess;
+export default withRouter(TeamLess);
